b26af755bd02da76758cbfece8b5e3f135e2bb57
Artur Calderon <artur.calderonart@gmail.com>
2022-02-26 11:12:39 -0400

    responsive

--- main.js
+++ main.js
@@ -2,17 +2,34 @@ const client = document.querySelector('.cliente')
 const loginPPPOE = document.querySelector('.loginPPPOE')
 const senhaPPPOE = document.querySelector('.senhaPPPOE')
 const senhaModem = document.querySelector('.senhaModem')
+
+const form = document.querySelector('.form')
+
 const btn_gerar = document.querySelector('.gerar')
 const btn_Liberacao = document.querySelector('.btn_liberacao') 
+const btn_reset = document.querySelector('.resetar') 
 
 
 btn_gerar.addEventListener('click',gerarPPPOE);
 btn_Liberacao.addEventListener('click',pedirLiberacao)
+btn_reset.addEventListener('click',(e)=>{
+  e.preventDefault()
+  form.reset()
+  const boxResults = document.querySelector('.results')
+  const contacts = document.querySelector('.contatosN2')
+  boxResults.innerHTML=''
+  boxResults.style.display = 'none'
+  contacts.style.display = 'none'
+})
 
 
 
 function gerarPPPOE(e){
   e.preventDefault();
+  if(client.value === '' || loginPPPOE === ' ' || senhaPPPOE === '' || senhaModem === '' ){
+    alert('Preencha os campos')
+    return
+  }
   let dados = {
     nomeCliente: client.value,
     loginPPPOE: loginPPPOE.value,
