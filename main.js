a8a7e1747d9c5ad92ed9052f3b6994b07efd3aea
Artur Calderon <artur.calderonart@gmail.com>
2022-03-21 00:36:10 -0400

    Password Generator and new fields

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ const client = document.querySelector('.cliente')
 const loginPPPOE = document.querySelector('.loginPPPOE')
 const senhaPPPOE = document.querySelector('.senhaPPPOE')
 const senhaModem = document.querySelector('.senhaModem')
+const codigoAparelho = document.querySelector('.codigoAparelho')
+const caixaCTO = document.querySelector('.caixaCTO')
+const porta = document.querySelector('.porta')
 
 const form = document.querySelector('.form')
 
@@ -22,6 +25,32 @@ btn_reset.addEventListener('click',(e)=>{
   contacts.style.display = 'none'
 })
 
+function GeraSenha(){
+  let maiusculas =['A','B','C', 'D', 'E', 'F' ,'G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','W','X','Y','Z']
+  let minusculas =['a','b','c', 'd', 'e', 'f' ,'g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','w','x','y','z']
+
+  let numeros =['0','1','2', '3', '4', '5' ,'6','7','8', '9']
+  
+  let senhaGerada = ''
+    for(let i=0; i<2; i++){
+
+      let sortMaiusculas = Math.floor(Math.random() * (25 - 0) + 0);
+      let sortMinusculas = Math.floor(Math.random() * (25 - 0) + 0);
+      let sortNumeros = Math.floor(Math.random() * (10 - 0) + 0);
+  
+      // console.log(maiusculas[sortMaiusculas])
+      // console.log(minusculas[sortMinusculas])
+      // console.log(numeros[sortNumeros])
+      senhaGerada += maiusculas[sortMaiusculas] + minusculas[sortMinusculas] + numeros[sortNumeros]
+    }
+    
+    return senhaGerada;
+}
+
+senhaModem.addEventListener('click',()=>{
+  senhaModem.value = GeraSenha()
+})
+
 
 
 function gerarPPPOE(e){
@@ -34,7 +63,11 @@ function gerarPPPOE(e){
     nomeCliente: client.value,
     loginPPPOE: loginPPPOE.value,
     senhaPPPOE: senhaPPPOE.value,
-    senhaModem: senhaModem.value
+    senhaModem: senhaModem.value,
+    codigoAparelho: codigoAparelho.value,
+    caixaCTO: caixaCTO.value,
+    porta: porta.value
+
   }
 
   console.log(dados)
@@ -50,6 +83,9 @@ function gerarPPPOE(e){
     <p><b>Login PPPoE:</b> ${dados.loginPPPOE}jpr </p>
     <p><b>Senha PPPoE:</b> ${dados.senhaPPPOE} </p>
     <p><b>Senha Modem:</b> Speed@${dados.senhaModem} </p>
+    <p><b>Código do Aparelho:</b>${dados.codigoAparelho} </p>
+    <p><b>Caixa:</b>${dados.caixaCTO} </p>
+    <p><b>Porta:</b>${dados.porta} </p>
   `
 }
 
@@ -61,7 +97,10 @@ function pedirLiberacao(e){
     nomeCliente: client.value,
     loginPPPOE: loginPPPOE.value,
     senhaPPPOE: senhaPPPOE.value,
-    senhaModem: senhaModem.value
+    senhaModem: senhaModem.value,
+    codigoAparelho: codigoAparelho.value,
+    caixaCTO: caixaCTO.value,
+    porta: porta.value
   }
   const text = " *Nome do Cliente:*  " + dados.nomeCliente + "\n" + "*Login PPPoE:* " + dados.loginPPPOE + "jpr" + "\n" + "*Senha PPPoE:*  " + dados.senhaPPPOE + "\n" + "*Senha Modem:*  " + "Speed@" + dados.senhaModem
  
